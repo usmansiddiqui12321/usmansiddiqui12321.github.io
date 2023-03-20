82565c3bac7f6bb0bb7544c9cd452d823c2716c0
usmansiddiqui12321 <usmansiddiqui12321@gmail.com>
2023-03-20 18:30:04 +0500

    projects.dart Update 1

--- lib/models/Project.dart
+++ lib/models/Project.dart
@@ -7,42 +7,41 @@ class Project {
 // ignore: non_constant_identifier_names
 List<Project> demo_projects = [
   Project(
-    details: "",
     title: "Responsive Admin Panel or Dashboard",
     description:
         "Flutter V2.* now officially supports web on a stable branch, which means you can use Flutter to build web apps. Recently, I created an Admin panel or dashboard UI using Flutter that you can use as a reference or starting point for your own project. With Flutter, you have the ability to create a dashboard that includes charts, tables, and small cards to display information in a visually appealing way. This Admin panel was designed to contain everything you need to get started with building your own dashboard. The UI is clean and intuitive, making it easy for users to navigate and find what they're looking for. With Flutter's powerful and flexible UI toolkit, you can easily customize and add new features to this dashboard to fit your specific needs. This Admin panel was built using Flutter's web support, so it's optimized for performance and works seamlessly across different screen sizes and devices. Whether you're building an internal tool or a customer-facing dashboard, Flutter provides a great platform to create high-quality and engaging UIs. So if you're looking to build a dashboard for your web app, give Flutter a try and see how easy it can be to create beautiful and functional interfaces.",
     videopath: "videos/covid.mp4",
   ),
   Project(
-    details: "",
+    
     title: "E-Commerce Complete App",
     description:
         "In the first part of our complete e-commerce app, we show you how you can create a nice clean onboarding screen for your e-commerce app that can run both Andriod and iOS devices because it builds with flutter. Then on the second episode, we build a Sign in, Forgot Password screen with a custom error indicator.",
     videopath: "videos/covid.mp4",
   ),
   Project(
-    details: "",
+    
     title: "Outlook Email App Redesign - Flutter Fully Responsive Design UI",
     description:
         "We redesign the outlook app also make it responsive so that you can run it everywhere on your phone, tab, or web. In this flutter responsive video, we will show you the real power of flutter. Make mobile, web, and desktop app from a single codebase.",
     videopath: "videos/covid.mp4",
   ),
   Project(
-    details: "",
+    
     title: "Chat/Messaging App Light and Dark Theme - Flutter UI",
     description:
         "Today we gonna build messing/chat app #ui using #flutter that runs both Android and iOS devices also has a dark and light theme. We create in total 4 screens all of that support both Dark Theme and Light Theme. At first, we design a welcome screen that contains an image with a tag line also has a skip button.",
     videopath: "videos/covid.mp4",
   ),
   Project(
-    details: "",
+    
     title: "Welcome page, Login Page and Sign up page - Flutter UI",
     description:
         "In the first part of our complete e-commerce app, we show you how you can create a nice clean onboarding screen for your e-commerce app that can run both Andriod and iOS devices because it builds with flutter. Then on the second episode, we build a Sign in, Forgot Password screen with a custom error indicator.",
     videopath: "videos/covid.mp4",
   ),
   Project(
-    details: "",
+    
     title: "Covid-19 App - Flutter UI",
     description:
         "We redesign the outlook app also make it responsive so that you can run it everywhere on your phone, tab, or web. In this flutter responsive video, we will show you the real power of flutter. Make mobile, web, and desktop app from a single codebase.",
